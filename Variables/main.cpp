b5f075f21d54378efd4a9e87f61d8abc006e0009
joshuanathanielhagan <joshuanathanielhagan@gmail.com>
2018-08-15 18:00:15 -0500

    Added Homework 1-3

--- Variables/main.cpp
+++ Variables/main.cpp
@@ -37,6 +37,60 @@ int main()
 	std::cin >> InputNumber;
 	std::cout << "Your Input:" << InputNumber << std::endl;
 
+	// A) Answer = 14
+	int numberA = 5;
+	numberA = 9;
+	numberA = 11;
+	numberA = 14;
+	std::cout << "A) " << numberA << std::endl;
+
+	// B) Answer = 9
+	int numberB = 10;
+	numberB = 9;
+	int thingB = 55;
+	thingB = 22;
+	std::cout << "B) " << numberB << std::endl;
+
+	// C) Answer = 21
+	int numberC = 3;
+	numberC = 7;
+	numberC = 1;
+	int somethingC = 23;
+	somethingC = 21;
+	numberC = somethingC;
+	std::cout << "C) " << numberC << std::endl;
+
+	// D) Answer = 3
+	int numberD = 1;
+	int somethingD = 3;
+	numberD = somethingD;
+	std::cout << "D) " << somethingD << std::endl;
+
+	// E) Answer = 6
+	int x = 13;
+	x = x / 2;
+	std::cout << "E) " << x << std::endl;
+	// F) Answer = 6.5
+	float y = 13;
+	y = y / 2;
+	std::cout << "F)" << y << std::endl;
+
+	
+	float degCelsius = 78.0f;
+	float degFahrenheit = 0.0f;
+	// 1.) Answer = 172.4
+	
+	std::cout << "Input degCelsius" << std::endl;
+	std::cin >> degCelsius;
+
+	float celsiusToFahrenheight = 
+
+	std::cout << "Celsius to Fahrenheit)" << std::endl;
+	std::cout << "Celsius: " << degCelsius << std::endl;
+	std::cout << "Fahrenheit: " << degFahrenheit << std::endl;
+	
+
+
 	system("pause"); 
 
 }
