1a00c9fde8eaec42a606aa1a644bea9de37641d4
joshuanathanielhagan <joshuanathanielhagan@gmail.com>
2018-08-24 12:41:21 -0500

    Added Loops

---
+++ Loops/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+int main()
+{
+	for (int i = 1; i <= 10; i++)
+	{
+		std::cout << i << std::endl;
+	}
+
+	//Using while to count to 10
+	int i = 1;
+	while (i <= 10)
+	{
+		std::cout << i << ", ";
+		i++;
+	}
+	std::cout << i << std::endl;
+
+	//Making program quit by pressing q or Q
+	char input;
+	do 
+	{
+		std::cout << "Press q or Q to quit this program" << std::endl;
+		std::cin >> input;
+	} while(input != 'q' && input != 'Q');
+
+
+	//Create a for loop printing only even numbers
+	for (int i = 2; i <= 10; i += 2)
+	{
+		std::cout << i << std::endl;
+	}
+	
+
+
+	system("pause");
+}
