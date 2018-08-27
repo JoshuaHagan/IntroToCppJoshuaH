4121248fbb3f771c8722aa174152aaaf307c0a0e
JoshuaHagan <42358820+JoshuaHagan@users.noreply.github.com>
2018-08-27 08:18:07 -0500

    Update main.cpp

--- Conditional/main.cpp
+++ Conditional/main.cpp
@@ -108,16 +108,222 @@ int main()
 
 		if (y == 0)
 		{
+			x = 100;
 
 		}
+		std::cout << "x = 100" << x << std::endl;
+	}
+	//2.) Write a program that asks the user for two numbers and then display the larger number to the console.
+	{
+		int x;
+		int y;
 
-		
+		if (x > y)
+		{
+			std::cout << "Enter x" << x << std::endl;
+			std::cin >> x;
+			std::cout << "Enter y" << y << std::endl;
+			std::cin >> y;
+
+		}
+		else (x < y);
 
 
 
-		
+
 
 
 	}
+	//3.)
 
+
+	//4.)Convert the following code into a switch statement
+	{
+		int choice;
+		switch (choice)
+		{
+		case 1:
+		{
+			std::cout << '1';
+		}
+		break;
+		case 2:
+		{
+			std::cout << "2 or 3";
+		}
+		break;
+		case 3:
+		{
+			std::cout << '4';
+		}
+		break;
+		default:
+		{
+			std::cout << "Invalid" << std::endl;
+		}
+		break;
+
+		}
+	} system("pause");
+
+
+
+
+	//5.)Convert the following if statement into a ternary operator
+
+
+
+
+
+
+
+	 //6.)Write a program that accepts from the user two numbers and a mathematical operation
+	//character(+, -, *, / , %).Perform the appropriate maths based on which character is input.
+	int lhs, rhs;
+	char op;
+	std::cout << "Enter two numbers.\n";
+	std::cin >> lhs >> rhs;
+	std::cout << "Do you wanna Add(+) or Subtract (-)?\n";
+	std::cin >> op;
+	switch (op)
+	{
+	case '+':
+
+		std::cout << lhs + rhs << std::endl;
+		break;
+	case '-':
+		std::cout << lhs - rhs << std::endl;
+		break;
+	case '*':
+		std::cout << lhs * rhs << std::endl;
+		break;
+	case '/':
+		std::cout << lhs / rhs << std::endl;
+		break;
+	case '%':
+		std::cout << lhs % rhs << std::endl;
+	default:
+		std::cout << "Invalid math operation\n";
+		break;
+	}
+	//7.)Write a program that accepts an integer that represents the month of the year.
+	//It should then display the number of days in that month. If a number that doesn’t correspond to a
+	//month is entered then the program should display an error message.
+	{
+
+
+		int choice;
+		switch (choice)
+		{
+		case 1:
+		{
+			std::cout << "Janurary - 31 days";
+		}
+		break;
+		case 2:
+		{
+			std::cout << "February - 28 days";
+		}
+		break;
+		case 3:
+		{
+			std::cout << "March - 31 days";
+		}
+		break;
+		case 4:
+		{
+			std::cout << "April - 31 days";
+		}
+		break;
+
+		case 5:
+		{
+			std::cout << "May - 31 days";
+		}
+		break;
+		case 6:
+		{
+			std::cout << "June - 31 days";
+		}
+		break;
+		case 7:
+		{
+			std::cout << "July - 31 days";
+		}
+		break;
+		case 8:
+		{
+			std::cout << "August - 31 days";
+		}
+		break;
+		case 9:
+		{
+			std::cout << "Spetemeber - 31 days";
+		}
+		break;
+		case 10:
+		{
+			std::cout << "October - 31 days";
+		}
+		break;
+		case 11:
+		{
+			std::cout << "November - 31 days";
+		}
+		break;
+		case 12:
+		{
+			std::cout << "December - 31 days";
+		}
+		break;
+		}
+	}
+	//8.)
+	{
+		"a.true";
+		"b.true";
+		"c.false";
+		"d.true";
+		"e.true";
+		"f.false";
+		"g.true";
+		"h.false";
+		"i.true";
+		"j.true";
+
+
+
+
+
+
+
+
+
+
+
+	}
+	//9.)
+	{
+		"a.std::cout << true";
+		"b.std::cout << false";
+		"c.std::cout << true";
+		"d.std::cout << false";
+		"e.std::cout << true";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	}
 }
