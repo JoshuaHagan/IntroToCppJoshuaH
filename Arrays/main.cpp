a9fc54b3b47c40e4ff4965ccd8ad56b020c432e5
joshuanathanielhagan <joshuanathanielhagan@gmail.com>
2018-08-27 17:50:28 -0500

    Added Homework

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -43,8 +43,51 @@ int main()
 	int thearrays[3][3] = { {1,2,3}, {4,5,6}, {7,8,9} };
 	for (int i = 0; i < 0; i++)
 	{
+		for(int j = 0; j < 3; j++)
+		std::cout << thearrays << std::endl;
+	}
+	  
+
+
+
+
+
+
+	//1.) Is each of the following a valid or invalid array definition?
+	{
+		std::cout << "valid";
+		std::cout << "Invalid";
+		std::cout << "Invalid";
+		std::cout << "Invalid";
+		std::cout << "valid";
+		std::cout << "valid";
+		std::cout << "valid";
+		std::cout << "valid";
+	}
+	
+	//2.)Given the following array definition.
+	//What does each of the following display?
+	{
+		std::cout << "a.10";
+		std::cout << "b.2";
+		std::cout << "c.6";
+		std::cout << "d.10";
+		std::cout << "e.It going to be invalid cause there is no 4th index";
 
 	}
+
+	//3.) Write a for loop to initialize the following array (int data[10]) with the values 10, 9, 8... (Etc)
+
+	int data[10];
+	for (int i = 0; i > 10; i--)
+	{
+		std::cout << data[i] << std::endl;
+	}
+
+
+
+
+
 	
 
 	system("pause");
