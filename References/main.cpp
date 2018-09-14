9d7df7d6778b1e2cee85e1bcf98c8313879f5347
joshuanathanielhagan <joshuanathanielhagan@gmail.com>
2018-09-14 12:53:35 -0500

    Added References

---
+++ References/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+
+int main()
+{
+	int var = 10;
+	std::cout << var << std::endl;
+	int &va = var;
+	std::cout << var << std::endl;
+	var = 8;
+
+
+
+
+
+
+
+	
+
+	system("pause");
+}
