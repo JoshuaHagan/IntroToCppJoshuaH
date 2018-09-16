4846d15766585b0e8f99cc3a31f5c60c90d7cce9
JoshuaHagan <42358820+JoshuaHagan@users.noreply.github.com>
2018-09-16 15:27:53 -0500

    Update main.cpp

--- References/main.cpp
+++ References/main.cpp
@@ -8,6 +8,40 @@ int main()
 	int &va = var;
 	std::cout << var << std::endl;
 	var = 8;
+	//g.)
+	int &va2 = var;
+	std::cout << va2 << var << std::endl;
+	//h.)
+
+
+
+
+
+	//2.)
+	//a.)
+	int x = 20;
+	int y = 40;
+	//b.)
+	std::cout << "x = " << x << std::endl;
+	std::cout << "y =" << y << std::endl;
+	//c.)
+	int& x = 20;
+	int& y = 40;
+	
+
+
+		
+
+
+
+
+	//3.)
+	//a.)
+	int x = 20;
+	int y = 40;
+	//b.)
+	
+
 
 
 
