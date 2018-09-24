0c5e74f9f4249264dbadd821ece8f826e2f4847e
JoshuaHagan <42358820+JoshuaHagan@users.noreply.github.com>
2018-09-24 07:25:49 -0500

    Update main.cpp

--- Lab/main.cpp
+++ Lab/main.cpp
@@ -8,6 +8,8 @@ int main()
 	Josh->Hero::Hero();
 	Lodis->Fight(*Josh);
 	Josh->Fight(*Lodis);
+	Game*g = new Game();
+	g -> Battle(Lodis, Josh);
 	std::cout << "Welcome to Hero Fight Game Prototype\n";
 	system("pause");
 }
