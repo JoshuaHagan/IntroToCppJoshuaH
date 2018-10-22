c654089b5429eb75922808764e9e1e0c9891edf0
joshuanathanielhagan <joshuanathanielhagan@gmail.com>
2018-10-22 15:39:14 -0500

    Define more

--- LinkedList/InsertingList.h
+++ LinkedList/InsertingList.h
@@ -12,19 +12,23 @@ class InsertingList:SearchList<R>
 template<class R>
  void InsertingList<R>::insertFirst(const R & n3)()
 {
-	 nodeType <R> * n3 = new nodeType<R>;
-	 if()
+	 nodeType <R> * n5 = new nodeType<R>;
+	 n5->info = R;	
+	 n5->link = first;
 }
 
  template<class R>
   void InsertingList<R>::insertLast(const R & n4)()
  {
-	  nodeType <R> * n4 = new nodeType<R>;
-	  if()
+	  nodeType <R> * n6 = new nodeType<R>;
+	  n6->info = R;
+	  n6->link = last;
  }
 
   template<class R>
    void InsertingList<R>::deleteNode(const R &)()
   {
-
+	   node = *n7;
+	   node = *n8;
+	   n7->first; 
   }
--- LinkedList/SearchList.h
+++ LinkedList/SearchList.h
@@ -4,16 +4,10 @@
 template <class S>
 class SearchList:CheckList<S>
 {
-	void destroyList() override;
 	Type front() override;
 	Type back() override;
 	bool search(const Type&);
 };
-
-template<class S>
- void SearchList<S>::destroyList()
-{
-}
 
  template<class S>
   bool SearchList<S>::search(const Type &)
