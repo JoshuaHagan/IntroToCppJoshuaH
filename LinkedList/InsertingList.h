304aca75fb1e5144efdf5e2e2b16e4dc61b65421
joshuanathanielhagan <joshuanathanielhagan@gmail.com>
2018-10-25 16:36:58 -0500

    defined new stuff working on Print

--- LinkedList/InsertingList.h
+++ LinkedList/InsertingList.h
@@ -1,33 +1,57 @@
 #pragma once
-#include "SearchList.h"
+#include "LinkedList.h"
 
 template <class R>
-class InsertingList:SearchList<R>
+class InsertingList:public linkedListType<R>
 {
-	void insertFirst(const R&) () override;
-	void insertLast(const R&)  () override;
-	void deleteNode(const R&) () override;
+public:
+	void insertFirst(const R&) override;
+	void insertLast(const R&) override;
+	void deleteNode(const R&) override;
+	void search(const R&) override;
 };
 
 template<class R>
- void InsertingList<R>::insertFirst(const R & n3)()
+void InsertingList<R>::insertFirst(const R & n3)
 {
-	 nodeType <R> * n5 = new nodeType<R>;
-	 n5->info = R;	
-	 n5->link = first;
+	nodeType <R> * n5 = new nodeType<R>;
+	n5->info = n3;
+	n5->link = this->first;
+	first = n5->link;
 }
 
  template<class R>
-  void InsertingList<R>::insertLast(const R & n4)()
+ void InsertingList<R>::insertLast(const R & n4)
  {
-	  nodeType <R> * n6 = new nodeType<R>;
-	  n6->info = R;
-	  n6->link = last;
+	 nodeType <R> * n6 = new nodeType<R>;
+	 n6->info = n4;
+	 n6->link = this->last;
+	 last = n6->link;
  }
+  //make it iterate through to find the one you want to delete
+	template<class R>
+	void InsertingList<R>::deleteNode(const R &dnode)
+	{
+		nodeType<Type>* ptr = first;
+		nodeType<Type>* tail = ptr;
+		while (ptr->info != dnode)
+		{
+			tail = ptr;
+			ptr = ptr->link;
+		}
+		tail->link = ptr->link;
+		delete ptr;
+	}
+   //Move if ptr.info != dnode
+   //	tail = ptr
+   //	ptr = ptr.link
+   //else
+   //	tail.link = ptr.link
+   //	delete ptr
+   //	return
 
-  template<class R>
-   void InsertingList<R>::deleteNode(const R &node)()
-  {
-	   node->nodeType<R>;
-	   nodeType<R>->delete;
-  }
+   template<class R>
+   void InsertingList<R>::search(const R &snode)
+   {
+
+   }
--- LinkedList/main.cpp
+++ LinkedList/main.cpp
@@ -1,8 +1,11 @@
 #include "LinkedList.h"
+#include "InsertingList.h"
+#include "node.h"
+#include "Iterator.h"
 
 int main()
 {
-	nodeType<int>*n1 = new nodeType<int>;
+	/*nodeType<int>*n1 = new nodeType<int>;
 	nodeType<int>*n2 = new nodeType<int>;
 	n1->info = 2;
 	n2->info = 3;
@@ -10,5 +13,13 @@ int main()
 	n2->link = nullptr;
 	linkedListIterator<int> iterate (n1);
 	++iterate;
-	int why = *iterate;
+	int why = *iterate;*/
+
+	nodeType<int>* n1 = new nodeType<int>{ 2 };
+	nodeType<int>* n2 = new nodeType<int>{ 4 };
+	nodeType<int>* n3 = new nodeType<int>{ 8 };
+	nodeType<int>* n4 = new nodeType<int>{ 12 };
+
+
+	system("pause");
 }
