3c39412a31ff9db9278dfce309dd29c9bbdea192
joshuanathanielhagan <joshuanathanielhagan@gmail.com>
2018-12-14 12:55:14 -0600

    working on linking vector

---
+++ dependecies/Mathlib/include/Vector3.h
@@ -0,0 +1,31 @@
+#pragma once
+
+
+class Vector3
+{
+private:
+	float xPos;
+	float yPos;
+	float zPos;
+public:
+	Vector3();
+	Vector3(float x, float y, float z);
+
+	float Magnitude();
+	float Normalize();
+	float Distance(Vector3& other);
+	float DotProd(Vector3 other);
+	Vector3 CrossProd(Vector3 other);
+
+	float GetX();
+	float GetY();
+	float GetZ();
+	//give the addition and subtraction for the vector
+	Vector3 operator + (Vector3& rhs);
+	Vector3 operator - (Vector3& rhs);
+	//give the multiplication for the vector
+	Vector3 operator * (float & rhs);
+
+	bool operator == (Vector3& rhs);
+	bool operator != (Vector3& rhs);
+};
